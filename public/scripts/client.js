8f37ded8df76da8c31ec0cd5b56e125a08327fb5
Your Name <your@email.com>
2023-04-26 18:09:31 -0600

    implemented error message for empty tweet or more than 140 characters and clean the textarea after submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,101 +7,101 @@
 // const tweets = require("../../server/routes/tweets");
 
 // Fake data taken from initial-tweets.json
-$(document).ready(function() {
-  const $tweetsSeccion = $('#tweets-container');
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-const createTweetElement = function (tweet) {
-  /* Your code for creating the tweet element */
-  const $tweet = $("<article>").addClass("tweet");
-  const $header = $("<header>");
-  const $avatarName = $("<div>").addClass("avatarName");
-  const $avatarNameTop = $("<div>").addClass("avatarNameTop");
-  const $avatar = $("<img>").addClass("avatar").attr("src", tweet.user.avatars).attr("alt", "Avatar");
-  const $userName = $("<h3>").addClass("user-name").text(tweet.user.name);
-  const $userHandle = $("<span>").addClass("user-handle").text(tweet.user.handle);
-  const $text = $("<p>").addClass("tweet-text").text(tweet.content.text);;
-  const $footer = $("<footer>");
-  const $date = $("<span>").addClass("tweet-date").text(tweet.created_at);
-  const $icons = $("<div>").addClass("tweet-icons");
-  const $flag = $("<i>").addClass("fa-solid fa-flag");
-  const $retweet = $("<i>").addClass("fa-solid fa-retweet");
-  const $heart = $("<i>").addClass("fa-solid fa-heart");
+$(document).ready(function () {
+  const $tweetsSeccion = $("#tweets-container");
 
-  $avatarNameTop.append($avatar, $userName);
-  $avatarName.append($avatarNameTop, $userHandle);
-  $header.append($avatarName, $text);
-  $icons.append($flag, $retweet, $heart);
-  $footer.append($date, $icons);
-  $tweet.append($header, $footer);
-  console.log($tweet);
+  const createTweetElement = function (tweet) {
+    /* Your code for creating the tweet element */
+    const $tweet = $("<article>").addClass("tweet");
+    const $header = $("<header>");
+    const $avatarName = $("<div>").addClass("avatarName");
+    const $avatarNameTop = $("<div>").addClass("avatarNameTop");
+    const $avatar = $("<img>").addClass("avatar").attr("src", tweet.user.avatars).attr("alt", "Avatar");
+    const $userName = $("<h3>").addClass("user-name").text(tweet.user.name);
+    const $userHandle = $("<span>").addClass("user-handle").text(tweet.user.handle);
+    const $text = $("<p>").addClass("tweet-text").text(tweet.content.text);
+    const $footer = $("<footer>");
+    const $date = $("<span>").addClass("tweet-date").text(timeago.format(tweet.created_at));
+    const $icons = $("<div>").addClass("tweet-icons");
+    const $flag = $("<i>").addClass("fa-solid fa-flag");
+    const $retweet = $("<i>").addClass("fa-solid fa-retweet");
+    const $heart = $("<i>").addClass("fa-solid fa-heart");
 
-  return $tweet;
-};
+    $avatarNameTop.append($avatar, $userName);
+    $avatarName.append($avatarNameTop, $userHandle);
+    $header.append($avatarName, $text);
+    $icons.append($flag, $retweet, $heart);
+    $footer.append($date, $icons);
+    $tweet.append($header, $footer);
+    console.log($tweet);
 
-const renderTweets = function (tweets) {
-  $tweetsSeccion.empty();
-  // loops through tweets
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet); // calls createTweetElement for each tweet
-    $("#tweets-container").prepend($tweet); // takes return value and appends it to the tweets container
-  }
-};
+    return $tweet;
+  };
 
-const fetchtweets = () => (
-  $.ajax({
-    method: 'GET',
-    url: '/tweets'
-  }).then((tweets) => {
-    renderTweets(tweets)
-  })
-)
+  const renderTweets = function (tweets) {
+    $tweetsSeccion.empty();
+    // loops through tweets
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet); // calls createTweetElement for each tweet
+      $("#tweets-container").prepend($tweet); // takes return value and appends it to the tweets container
+    }
+  };
 
-// renderTweets(data);
-fetchtweets();
+  const loadTweets = () =>
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+    }).then((tweets) => {
+      renderTweets(tweets);
+    });
 
-const $form = $('#new-tweet-form');
+  // renderTweets(data);
+  loadTweets();
 
-$form.on('submit', (event) => {
-  event.preventDefault();
-  console.log('Form submited')
-  const dataTweet = $form.serialize();
-  console.log(dataTweet);
+  const $form = $("#new-tweet-form");
+  const $textArea = $form.find('textarea');
+  const errorMessage = $('#error-message');
 
-  $.ajax({
-    method: 'POST',
-    url: '/tweets',
-    data: dataTweet
-  }).then(() => {
-    console.log('request right');
-    fetchtweets();
-  })
+  // Moved event listeners for the textarea and form outside the submit event listener
+  $textArea.on('input', function() {
+    errorMessage.hide();
+  });
+
+  $form.on("submit", (event) => {
+    event.preventDefault();
+    const $textArea = $form.find("textarea");
+    const textTweet = $textArea.val().trim();
+    if(!textTweet){
+      const $errorEmpty = $("<h2>").text("Tweet is empty");
+      errorMessage.empty().append($errorEmpty);
+      errorMessage.show(); // Show the error message
+      return;
+    }
+    if (textTweet.length > 140){
+      const $errorLonger = $("<h2>").text("The tweet is longer than 140 characters.");
+      errorMessage.empty().append($errorLonger);
+      errorMessage.show(); // Show the error message     
+      return;
+    }
+   
+    const dataTweet = $form.serialize();
+    console.log(dataTweet);
 
 
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: dataTweet,
+    }).then(() => {
+      console.log("request right");
+      loadTweets();
+      $textArea.val("");
+    });
 
-})
-
-
+    $textArea.on("input", () => {
+      $("#errorMessage").empty();
+    });
+  });
+  errorMessage.hide(); // Hide the error message after successful submission
+  
 });
